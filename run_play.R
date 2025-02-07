268142b7ebd80975d41cdb04c633403be0405df5
JJSutkus <awj8sf@virginia.edu>
2025-02-07 10:48:05 -0500

    Update run_play.R

--- run_play.R
+++ run_play.R
@@ -1,14 +1,16 @@
 run_play <- function(down,ytg,fp){
-  YG <- yards_gained
-  if (YG < ytg){
-    down = down + 1
-    ytg = ytg - YG
-    fp = fp + YG
+  YG <- round(runif(1,-5,20))
+  if (down < 4){
+    if (YG < ytg){
+      down = down + 1
+      ytg = ytg - YG
+      fp = fp + YG
+    }
+    else{
+      down = 1
+      ytg = 10
+      fp = fp + YG
+    }
+  list(down,ytg,fp)
   }
-  else{
-    down = 1
-    ytg = 10
-    fp = fp + YG
-  }
-list(down,ytg,fp)
 }
