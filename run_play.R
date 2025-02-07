b0ecfcf4efd9cff1a0d0f086f46a79e513f6f7d8
JJSutkus <awj8sf@virginia.edu>
2025-02-07 10:32:24 -0500

    Add files via upload

---
+++ run_play.R
@@ -0,0 +1,14 @@
+run_play <- function(down,ytg,fp){
+  YG <- yards_gained
+  if (YG < ytg){
+    down = down + 1
+    ytg = ytg - YG
+    fp = fp + YG
+  }
+  else{
+    down = 1
+    ytg = 10
+    fp = fp + YG
+  }
+list(down,ytg,fp)
+}
