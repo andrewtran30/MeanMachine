a20b9fea83129b41fbeddfd725b1241686db32eb
zainkbangash <157034793+zainkbangash@users.noreply.github.com>
2025-03-28 10:29:50 -0400

    finalized mixture model implementation

--- updated_run_play.R
+++ updated_run_play.R
@@ -1,8 +1,9 @@
 source("FG_FD.R")  # For 4th down decision model
-source("yards_gained_em.R")  # Our EM-based mixture model
+source("yards_gained_em.R")  # Our mixture model
 
+# The main run_play function
 run_play <- function(down, ytg, fp) {
-  # Use our EM-based mixture model to generate yards gained
+  # Use our mixture model to generate yards gained
   result <- sample_yards_gained(down, ytg, fp)
   YG <- result$yards
   event_type <- result$event_type
@@ -18,10 +19,10 @@ run_play <- function(down, ytg, fp) {
   if (down < 4) {
     if (YG < ytg) {
       # Didn't make first down - increment down
-      list(down = down + 1, ytg = ytg - YG, fp = new_fp, exit_drive = 0)
+      return(list(down = down + 1, ytg = ytg - YG, fp = new_fp, exit_drive = 0))
     } else {
       # Made first down - reset to 1st & 10
-      list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 0)
+      return(list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 0))
     }
   } else {
     # 4th down decision using our statistical model
@@ -31,20 +32,20 @@ run_play <- function(down, ytg, fp) {
       # Use logistic regression model to determine success probability
       success_prob <- predict_fg_success(fp)
       if (runif(1) < success_prob) {
-        list(down = 1, ytg = 10, fp = 115, exit_drive = 1)  # Made field goal
+        return(list(down = 1, ytg = 10, fp = 115, exit_drive = 1))  # Made field goal
       } else {
-        list(down = 1, ytg = min(10, 100 - fp), fp = fp, exit_drive = 1)  # Missed
+        return(list(down = 1, ytg = min(10, 100 - fp), fp = fp, exit_drive = 1))  # Missed
       }
     } else if (decision == "go_for_it") {
       if (YG >= ytg) {
-        list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 0)  # Converted
+        return(list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 0))  # Converted
       } else {
-        list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 1)  # Failed
+        return(list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 1))  # Failed
       }
     } else {  # punt
       punt_distance <- sample(30:60, 1)
       new_fp <- max(0, min(120, 120 - (fp + punt_distance)))
-      list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 1)
+      return(list(down = 1, ytg = min(10, 100 - new_fp), fp = new_fp, exit_drive = 1))
     }
   }
 }
